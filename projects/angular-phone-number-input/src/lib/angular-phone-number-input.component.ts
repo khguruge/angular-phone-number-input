60610d459a2226b2d766e971b5adc9aa44e75143
khguruge <kasun@revox.io>
2023-12-27 23:21:30 +0530

    feat: Add phone number input library

---
+++ projects/angular-phone-number-input/src/lib/angular-phone-number-input.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-phone-number-input',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      angular-phone-number-input works!
+    </p>
+  `,
+  styles: ``
+})
+export class AngularPhoneNumberInput {
+
+}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-phone-number-input' title`, () => {
+  it(`should have the 'angular-phone-number-input-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-phone-number-input');
+    expect(app.title).toEqual('angular-phone-number-input-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-phone-number-input');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-phone-number-input-app');
   });
 });
